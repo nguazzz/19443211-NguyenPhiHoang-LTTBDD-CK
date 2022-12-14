6f86302b22c157cfafb693b0db4afad2f4877623
nguazzz <kuhoangkt@gmail.com>
2022-12-14 14:18:04 +0700

    finish cau 3

--- src/views/screens/DetailsScreen.js
+++ src/views/screens/DetailsScreen.js
@@ -1,110 +1,110 @@
-// import React from "react";
-// import { SafeAreaView, StyleSheet, View, Text, Image } from "react-native";
-// import { ScrollView } from "react-native-gesture-handler";
-// import Icon from "react-native-vector-icons/MaterialIcons";
-// import COLORS from "../../consts/colors";
-// import { PrimaryButton } from "../components/Button";
+import React from "react";
+import { SafeAreaView, StyleSheet, View, Text, Image } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import COLORS from "../../consts/colors";
+import { PrimaryButton } from "../components/Button";
 
-// const DetailsScreen = ({ navigation, route }) => {
-//   const item = route.params;
+const DetailsScreen = ({ navigation, route }) => {
+  const item = route.params;
 
-//   return (
-//     <SafeAreaView style={{ backgroundColor: COLORS.white }}>
-//       <View style={style.header}>
-//         <Icon name="arrow-back-ios" size={28} onPress={navigation.goBack} />
-//         <Text style={{ fontSize: 20, fontWeight: "bold" }}>Details</Text>
-//       </View>
-//       <ScrollView showsVerticalScrollIndicator={false}>
-//         <View
-//           style={{
-//             justifyContent: "center",
-//             alignItems: "center",
-//             height: 280,
-//           }}
-//         >
-//           <Image
-//             source={item.image}
-//             style={{ height: 200, width: 320, resizeMode: "contain" }}
-//           />
-//         </View>
-//         <View style={style.details}>
-//           <View
-//             style={{
-//               flexDirection: "row",
-//               justifyContent: "space-between",
-//               alignItems: "center",
-//             }}
-//           >
-//             <Text
-//               style={{ fontSize: 25, fontWeight: "bold", color: COLORS.white }}
-//             >
-//               {item.name}
-//             </Text>
-//             <View style={style.iconContainer}>
-//               <Icon name="favorite-border" color={COLORS.primary} size={25} />
-//             </View>
-//           </View>
-//           <View style={{ flexDirection: "row" }}>
-//             <Text
-//               style={{ fontSize: 15, fontWeight: "bold", color: COLORS.white }}
-//             >
-//               15% OFF | 2500$ --
-//             </Text>
-//             <Text
-//               style={{ fontSize: 15, fontWeight: "bold", color: COLORS.white }}
-//             >
-//               {item.price}
-//             </Text>
-//           </View>
-//           <Text style={style.detailsText}>
-//             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-//             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-//             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-//             aliquip ex ea commodo consequat. Duis aute irure dolor in
-//             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-//             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-//             culpa qui officia deserunt mollit anim id est laborum.
-//           </Text>
-//           <View
-//             style={{ marginTop: 40, marginBottom: 40, alignItems: "center" }}
-//           >
-//             <PrimaryButton title="Add To Cart" />
-//           </View>
-//         </View>
-//       </ScrollView>
-//     </SafeAreaView>
-//   );
-// };
+  return (
+    <SafeAreaView style={{ backgroundColor: COLORS.white }}>
+      <View style={style.header}>
+        <Icon name="arrow-back-ios" size={28} onPress={navigation.goBack} />
+        <Text style={{ fontSize: 20, fontWeight: "bold" }}>Details</Text>
+      </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            height: 280,
+          }}
+        >
+          <Image
+            source={item.image}
+            style={{ height: 200, width: 320, resizeMode: "contain" }}
+          />
+        </View>
+        <View style={style.details}>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Text
+              style={{ fontSize: 25, fontWeight: "bold", color: COLORS.white }}
+            >
+              {item.name}
+            </Text>
+            <View style={style.iconContainer}>
+              <Icon name="favorite-border" color={COLORS.primary} size={25} />
+            </View>
+          </View>
+          <View style={{ flexDirection: "row" }}>
+            <Text
+              style={{ fontSize: 15, fontWeight: "bold", color: COLORS.white }}
+            >
+              15% OFF | 2500$ --
+            </Text>
+            <Text
+              style={{ fontSize: 15, fontWeight: "bold", color: COLORS.white }}
+            >
+              {item.price}
+            </Text>
+          </View>
+          <Text style={style.detailsText}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Text>
+          <View
+            style={{ marginTop: 40, marginBottom: 40, alignItems: "center" }}
+          >
+            <PrimaryButton title="Add To Cart" />
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-// const style = StyleSheet.create({
-//   header: {
-//     paddingVertical: 20,
-//     flexDirection: "row",
-//     alignItems: "center",
-//     marginHorizontal: 20,
-//   },
-//   details: {
-//     paddingHorizontal: 20,
-//     paddingTop: 40,
-//     paddingBottom: 60,
-//     backgroundColor: COLORS.primary,
-//     borderTopRightRadius: 40,
-//     borderTopLeftRadius: 40,
-//   },
-//   iconContainer: {
-//     backgroundColor: COLORS.white,
-//     height: 50,
-//     width: 50,
-//     justifyContent: "center",
-//     alignItems: "center",
-//     borderRadius: 30,
-//   },
-//   detailsText: {
-//     marginTop: 10,
-//     lineHeight: 22,
-//     fontSize: 16,
-//     color: COLORS.white,
-//   },
-// });
+const style = StyleSheet.create({
+  header: {
+    paddingVertical: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    marginHorizontal: 20,
+  },
+  details: {
+    paddingHorizontal: 20,
+    paddingTop: 40,
+    paddingBottom: 60,
+    backgroundColor: COLORS.primary,
+    borderTopRightRadius: 40,
+    borderTopLeftRadius: 40,
+  },
+  iconContainer: {
+    backgroundColor: COLORS.white,
+    height: 50,
+    width: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 30,
+  },
+  detailsText: {
+    marginTop: 10,
+    lineHeight: 22,
+    fontSize: 16,
+    color: COLORS.white,
+  },
+});
 
-// export default DetailsScreen;
+export default DetailsScreen;
